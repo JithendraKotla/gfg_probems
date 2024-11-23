044b663002a80274245362ae239bd046a28fbd74
JithendraKotla <160527662+JithendraKotla@users.noreply.github.com>
2024-11-23 15:37:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing Number/smallest-positive-missing-number.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the smallest positive number missing from the array.
+    def missingNumber(self,arr):
+        #Your code here
+        arr=sorted(arr)
+        k=1
+        for i in arr:
+            if i>0:
+                if i>k:
+                    return k
+                elif i==k:
+                    k+=1
+        return k           
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+        arr = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.missingNumber(arr))
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
