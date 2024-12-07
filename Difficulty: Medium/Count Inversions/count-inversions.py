a23d3c40f3d2a986f5c5d4845ffcdc8084a5651d
JithendraKotla <160527662+JithendraKotla@users.noreply.github.com>
2024-12-07 23:31:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.py
@@ -0,0 +1,75 @@
+class Solution:
+    def inversionCount(self, arr):
+        def merge_and_count(temp, left, mid, right):
+            i = left
+            j = mid + 1
+            k = left
+            inv_count = 0
+
+            while i <= mid and j <= right:
+                if arr[i] <= arr[j]:
+                    temp[k] = arr[i]
+                    i += 1
+                else:
+                    temp[k] = arr[j]
+                    inv_count += (mid - i + 1)
+                    j += 1
+                k += 1
+
+            while i <= mid:
+                temp[k] = arr[i]
+                i += 1
+                k += 1
+
+            while j <= right:
+                temp[k] = arr[j]
+                j += 1
+                k += 1
+
+            for i in range(left, right + 1):
+                arr[i] = temp[i]
+
+            return inv_count
+
+        def merge_sort_and_count(temp, left, right):
+            inv_count = 0
+            if left < right:
+                mid = (left + right) // 2
+                inv_count += merge_sort_and_count(temp, left, mid)
+                inv_count += merge_sort_and_count(temp, mid + 1, right)
+                inv_count += merge_and_count(temp, left, mid, right)
+            return inv_count
+
+        n = len(arr)
+        temp = [0] * n
+        return merge_sort_and_count(temp, 0, n - 1)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        a = list(map(int, input().strip().split()))
+        obj = Solution()
+        print(obj.inversionCount(a))
+        print("~")
+
+# } Driver Code Ends
