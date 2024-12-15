ec64d4d25ec7089d3f173f97b2a9a6f7dc81cabd
JithendraKotla <160527662+JithendraKotla@users.noreply.github.com>
2024-12-15 22:52:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.py
+++ Difficulty: Basic/Peak element/peak-element.py
@@ -1,42 +1,58 @@
-# your task is to complete this function
-# function should return index to the any valid peak element
+
 class Solution:   
-    def peakElement(self,arr, n):
+    def peakElement(self,arr):
         # Code here
-        a=max(arr)
-        return arr.index(a)
+        n = len(arr)
         
+     
+        if n == 1:
+            return 0
+        
+     
+        if arr[0] > arr[1]:
+            return 0
+        if arr[-1] > arr[-2]:
+            return n-1
+        
+        for i in range(1, n - 1):
+            if arr[i - 1] < arr[i] and arr[i] > arr[i + 1]:
+                return i
+        
+        return -1
 
 
 
 #{ 
  # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())  # Read number of test cases
+    for _ in range(t):
+        # Read input and split it into a list of integers
+        arr = list(map(int, input().split()))
+        # Create a Solution object and calculate the result
 
-if __name__=='__main__':
-    t = int(input())
-    for i in range(t):
-        n = int(input())
-        arr = list(map(int, input().strip().split()))
-        index = Solution().peakElement(arr.copy(), n)
+        index = Solution().peakElement(arr)
+        n = len(arr)
         flag = False
-        if index<0 or index>=n:
+        if index < 0 or index >= n:
             flag = False
         else:
-            if index == 0 and n==1:
+            if index == 0 and n == 1:
                 flag = True
-            elif index==0 and arr[index]>=arr[index+1]:
+            elif index == 0 and arr[index] > arr[index + 1]:
                 flag = True
-            elif index==n-1 and arr[index]>=arr[index-1]:
+            elif index == n - 1 and arr[index] > arr[index - 1]:
                 flag = True
-            elif arr[index-1] <= arr[index] and arr[index] >= arr[index+1]:
+            elif index > 0 and index < n - 1 and arr[
+                    index - 1] < arr[index] and arr[index] > arr[index + 1]:
                 flag = True
             else:
                 flag = False
 
         if flag:
-            print(1)
+            print("true")
         else:
-            print(0)
-# Contributed by: Harshit Sidhwa
+            print("false")
+        print("~")
 
 # } Driver Code Ends
