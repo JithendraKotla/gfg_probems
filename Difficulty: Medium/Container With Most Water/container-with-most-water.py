2e981432f0d146ba5bbca443610900144390c0a7
JithendraKotla <160527662+JithendraKotla@users.noreply.github.com>
2025-01-13 23:12:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.py
@@ -0,0 +1,46 @@
+
+class Solution:
+    def maxWater(self, arr):
+        # code here
+        left=0
+        right=len(arr)-1
+        res=0
+        
+        
+        while left<right:
+            
+            
+            amount=min(arr[left],arr[right])*(right-left)
+            
+            res=max(res,amount)
+            
+            
+            if arr[left]<arr[right]:
+                left+=1
+            else:
+                right-=1
+        return res
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python 3
+
+import math
+
+
+def main():
+    t = int(input())
+    while (t > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+        obj = Solution()
+        print(obj.maxWater(arr))
+
+        t -= 1
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
